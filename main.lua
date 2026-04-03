3918c52f7267f8d67ef7115082f720df50891239
Erik Kronquist <erik.kronquist@gmail.com>
2026-04-03 12:25:50 -0600

    Add more to Q1Report and test basic Lua functionality

--- main.lua
+++ main.lua
@@ -1,5 +1,12 @@
-function hello_world()
-    print("Hello, World!")
-end
+local f = assert(io.open("socialsent.csv", "r"))
+local t = f:read("*line")
+t = f:read("*line")
+f:close()
 
-hello_world()
+local wordPattern = ".*,"
+local scorePattern = ",.+,"
+i, j = string.find(t, wordPattern)
+local word = 
+
+print(t)
+print(#t)
