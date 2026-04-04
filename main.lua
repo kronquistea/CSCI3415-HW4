64d31fec2fc8ddd7d6e42acf71ed9a430e89c097
Erik Kronquist <erik.kronquist@gmail.com>
2026-04-03 20:15:04 -0600

    Parse social sent scores csv file into table

--- main.lua
+++ main.lua
@@ -1,12 +1,16 @@
+local wordPattern = "[^,]*,"
+local scorePattern = ",.*,"
+
 local f = assert(io.open("socialsent.csv", "r"))
-local t = f:read("*line")
-t = f:read("*line")
+local wordsAndScores = {}
+
+for line in f:lines() do
+    local i, j = string.find(line, wordPattern)
+    local word = string.sub(line, i, j-1)
+    local k, x = string.find(line, scorePattern)
+    local score = string.sub(line, k+1, x-2)
+    print(word, score)
+    wordsAndScores[word] = score
+end
+
 f:close()
-
-local wordPattern = ".*,"
-local scorePattern = ",.+,"
-i, j = string.find(t, wordPattern)
-local word = 
-
-print(t)
-print(#t)
