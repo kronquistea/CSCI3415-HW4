2de51138ded9b64dc20d18c138c87537d4a8d942
Erik Kronquist <erik.kronquist@gmail.com>
2026-04-04 00:02:14 -0600

    Add getSocialSentimentScore function

--- main.lua
+++ main.lua
@@ -12,6 +12,9 @@ function buildSocialSentimentTable(filename)
     -- Open social score file in read mode
     local f = assert(io.open(filename, "r"))
 
+    -- Read and discard the first line of the file (stanford link stuff)
+    print("Removing: ", f:read())
+
     -- Loop through each line in the file 
     for line in f:lines() do
         -- Get starting and ending index of part of line that fulfills the word pattern (inclusive)
@@ -36,10 +39,55 @@ end
 
 -- Function that takes the input file and generates a social sent score based on the SocialSentimentScores table
 function getSocialSentimentScore(filename)
+    -- Open file to analyze for scores
+    local f = assert(io.open(filename, "r"))
 
+    local t = f:read("*all")
+
+    -- Pattern to match at least one alphanumeric character
+    local wordPattern = "%w+"
+
+    -- Index for starting pattern matching from
+    local startPatternIndex = 1
+
+    -- Loop control counter
+    local i = 1
+    -- Loop through entire "file" (text string), singling out all words and putting them into a table (as an array)
+    while i ~= #t do
+        -- Find a word (or number) starting from index i in the string
+        local j, k = string.find(t, wordPattern, startPatternIndex)
+        print("i, j: ", j, k)
+        
+        -- Create empty string to be populated as substring from text file
+        local word = ""
+        -- Make sure i is populated (not nil)
+        if j then
+            -- Set word variable to be substring of text file according to matched pattern
+            word = string.sub(t, j, k)
+        end
+
+        -- Make sure k is populated (not nil)
+        if k then
+            -- Set starting index for pattern searching to end of current word/number found + 1 (next character)
+            startPatternIndex = k + 1
+
+            -- Set i (while loop control counter) to character after k
+            i = k + 1
+        -- If k was not a number (meaning no pattern was found), increment i
+        else
+            -- Increment i
+            i = i + 1
+        end
+
+        print("Word: ", word, "\n")
+    end
 end
 
 -- Function that generates the star rating for 
 function getStarRating()
 
 end
+
+buildSocialSentimentTable("socialsent.csv")
+
+getSocialSentimentScore("good.txt")
