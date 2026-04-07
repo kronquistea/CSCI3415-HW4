48db679ac8c8da514b82dea51e6cdb2642880c01
Erik Kronquist <erik.kronquist@gmail.com>
2026-04-06 22:04:52 -0600

    Create new buildSocialSentimentTablegMatch function to be simpler version of old buildSocialSentimentTable function

--- main.lua
+++ main.lua
@@ -15,23 +15,17 @@ local gWordsWithScoresFound = 0
 
 -- Function that builds the SocialSentimentScores table from the socialsent.csv file
 function buildSocialSentimentTable(filename)
-    -- Create pattern to match "words" starting with any character other than a comma, then "ending" with a comma
-    local wordPattern = "[^,]*,"
-
-    -- Create pattern to match "scores" starting with a command, then any number of characters then ending with a comma
-    local scorePattern = ",.*,"
-
     -- Open social score file in read mode
     local f = assert(io.open(filename, "r"))
 
     -- Read and discard the first line of the file (stanford link stuff)
-    print(string.format("Removing: %s", f:read()))
-    print()
+    print(string.format("Removing: %s\n", f:read()))
 
     -- Loop through each line in the file 
     for line in f:lines() do
         -- Get starting and ending index of part of line that fulfills the word pattern (inclusive)
-        local i, j = string.find(line, wordPattern)
+        local i, j = string.find(line, "[^,]*,")
+
         -- Get the substring of the word without ending comma and save it to "word" variable
         local word = ""
         -- Make sure the string.find function returned an index
@@ -40,15 +34,14 @@ function buildSocialSentimentTable(filename)
         end
         
         -- Get starting and ending index of part of line that fulfills the score pattern (inclusive)
-        local k, x = string.find(line, scorePattern)
+        local k, x = string.find(line, ",.*,")
+
         -- Get the substring of the score without starting command and two ending commas and save it to "score" variable
         local score = ""
         -- Make sure the string.find function returned an index
         if k ~= nil then
             score = string.sub(line, k+1, x-2)
         end
-
-        -- print(word, score)
 
         -- Store key (word) and value (score) in the wordsAndScores table (as a dictionary)
         gSocialSentimentScores[word] = score
@@ -58,15 +51,32 @@ function buildSocialSentimentTable(filename)
     f:close()
 end
 
+-- Function to build the social sentiment table using the gmatch lua function
+function buildSocialSentimentTablegMatch(socialSentFileName)
+    -- Open csv file in read mode
+    local csv = assert(io.open(socialSentFileName, "r"))
+    
+    -- Inform user that the first line (word,Sentiment scores,,https://nlp.stanford.edu/projects/socialsent/) is being removed for processing
+    print(string.format("Removing %s\n", csv:read()))
+
+    -- Loop through each line in the csv file
+    for line in csv:lines() do
+        -- Get the word and score capturing anything but a comma until a comma and then again anything but a comma
+        local word, score = line:match("^([^,]+),([^,]+)")
+
+        -- Convert score to a number
+        score = tonumber(score)
+
+        -- Store word and score in dictionary
+        gSocialSentimentScores[word] = score
+    end
+end
+
 -- Function that takes the input file and generates a social sent score based on the SocialSentimentScores table
 function getSocialSentimentScore(inputfile)
     -- Populate the scores for the words from the given input file
-    -- print("Generate Word Scores")
-    -- local wordsAndOccurrences = generateWordScores(inputfile)
-    -- print()
-
     print("Generating Word Scores")
-    local wordsAndOccurrences = generateWordScoresgMatch(inputfile)
+    local wordsAndOccurrences = generateWordOccurrencesgMatch(inputfile)
     print()
 
     -- Calculate the social sent score for the given input file
@@ -77,82 +87,8 @@ function getSocialSentimentScore(inputfile)
     gAccumulatedSocialSentScore = socialSentScore
 end
 
--- Function that generates scores for words based on SocialSentimentScores table
-function generateWordScores(inputfile)
-    -- Open file to analyze for scores
-    local f = assert(io.open(inputfile, "r"))
-
-    -- Read entire file and set to string variable t
-    local t = f:read("*all")
-
-    -- Table containing words and the number of occurrences for that word <word, occurrences>
-    local wordsAndOccurrences = {}
-
-    -- Pattern to match at strings with alphanumeric characters, apostrophes and right-side single quotes
-    local wordPattern = "[%w'‘’]+"
-
-    -- Index for starting pattern matching from
-    local startPatternIndex = 1
-
-    -- Loop control counter
-    local i = 1
-    -- Loop through entire "file" (text string), singling out all words and putting them into a table (as an array)
-    while i ~= #t do
-        -- Find a word (or number) starting from index i in the string
-        local j, k = string.find(t, wordPattern, startPatternIndex)
-
-        -- print("i, j: ", j, k)
-        
-        -- Create empty string to be populated as substring from text file
-        local word = ""
-        -- If j is populated (starting index was found) set word variable to substring from text "t"
-        if j then
-            -- Set word variable to be substring of text file according to matched pattern
-            word = string.sub(t, j, k)
-
-            -- Convert word to lowercase to ensure all words are matched properly with global SocialSentimentScores table
-            word = string.lower(word)
-        end
-
-        -- If k is populated (ending index was found) set new starting pattern index and increment i
-        if k then
-            -- Set starting index for pattern searching to end of current word/number found + 1 (next character)
-            startPatternIndex = k + 1
-
-            -- Set i (while loop control counter) to character after k
-            i = k + 1
-
-            -- If the word has a score, add the word and score to the list of wordsAndScores, otherwise skip the word,
-            -- For example, the word "this" has no score in the socialsent.csv file
-            if gSocialSentimentScores[word] then
-                -- If the number of occurrences is at least one (meaning not nil) then increment the number of occurrences for the word
-                -- wordsAndScores[word] will initally be nil (which results in false) until the else statement is executed
-                if wordsAndOccurrences[word] then
-                    -- Increment value for the word
-                    wordsAndOccurrences[word] = wordsAndOccurrences[word] + 1
-                else
-                    -- Set the number of occurrences for the word to 1 (first occurrence of word was found)
-                    wordsAndOccurrences[word] = 1
-                end
-
-                -- Increment the number of words with scores found (for end of output file purposes)
-                gWordsWithScoresFound = gWordsWithScoresFound + 1
-            end
-
-            -- Increment the number of total words found (with or without scores) (for end of output file purposes)
-            gTotalWordCount = gTotalWordCount + 1
-        -- If k was not a number (meaning no pattern was found), increment i
-        else
-            -- Increment i
-            i = i + 1
-        end
-    end
-
-    -- Return dictionary containing words from text file and number of occurrences
-    return wordsAndOccurrences
-end
-
-function generateWordScoresgMatch(inputfile)
+-- Function to generate the number of occurrences for the words in the input file using the gmatch lua function
+function generateWordOccurrencesgMatch(inputfile)
     -- Open input file in read mode
     local f = assert(io.open(inputfile, "r"))
 
@@ -300,7 +236,8 @@ function main()
     local inputfile = checkArgsAndGetInputfile()
 
     -- Build the social sentiment table baesd on "socialsent.csv"
-    buildSocialSentimentTable("socialsent.csv")
+    -- buildSocialSentimentTable("socialsent.csv")
+    buildSocialSentimentTablegMatch("socialsent.csv")
 
     -- Get the social sentiment score based on user input file
     getSocialSentimentScore(inputfile)
