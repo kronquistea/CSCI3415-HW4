8fa255ec1914c142627e13f3897262730f554df3
Erik Kronquist <erik.kronquist@gmail.com>
2026-04-06 21:45:33 -0600

    Create better generate word scores function and separate more code from main function

--- main.lua
+++ main.lua
@@ -33,12 +33,20 @@ function buildSocialSentimentTable(filename)
         -- Get starting and ending index of part of line that fulfills the word pattern (inclusive)
         local i, j = string.find(line, wordPattern)
         -- Get the substring of the word without ending comma and save it to "word" variable
-        local word = string.sub(line, i, j-1)
-
+        local word = ""
+        -- Make sure the string.find function returned an index
+        if i ~= nil then
+            word = string.sub(line, i, j-1)
+        end
+        
         -- Get starting and ending index of part of line that fulfills the score pattern (inclusive)
         local k, x = string.find(line, scorePattern)
         -- Get the substring of the score without starting command and two ending commas and save it to "score" variable
-        local score = string.sub(line, k+1, x-2)
+        local score = ""
+        -- Make sure the string.find function returned an index
+        if k ~= nil then
+            score = string.sub(line, k+1, x-2)
+        end
 
         -- print(word, score)
 
@@ -51,24 +59,28 @@ function buildSocialSentimentTable(filename)
 end
 
 -- Function that takes the input file and generates a social sent score based on the SocialSentimentScores table
-function getSocialSentimentScore(filename)
+function getSocialSentimentScore(inputfile)
     -- Populate the scores for the words from the given input file
-    print("Generate Word Scores")
-    local wordsAndOccurrences = generateWordScores(filename)
+    -- print("Generate Word Scores")
+    -- local wordsAndOccurrences = generateWordScores(inputfile)
+    -- print()
+
+    print("Generating Word Scores")
+    local wordsAndOccurrences = generateWordScoresgMatch(inputfile)
     print()
 
     -- Calculate the social sent score for the given input file
     print("Calculate Social Sentiment Score")
-    local socialSentScore = calculateSocialSentimentScore(wordsAndOccurrences, filename)
+    local socialSentScore = calculateSocialSentimentScore(wordsAndOccurrences, inputfile)
 
     -- Assign global variable
     gAccumulatedSocialSentScore = socialSentScore
 end
 
 -- Function that generates scores for words based on SocialSentimentScores table
-function generateWordScores(filename)
+function generateWordScores(inputfile)
     -- Open file to analyze for scores
-    local f = assert(io.open(filename, "r"))
+    local f = assert(io.open(inputfile, "r"))
 
     -- Read entire file and set to string variable t
     local t = f:read("*all")
@@ -140,11 +152,49 @@ function generateWordScores(filename)
     return wordsAndOccurrences
 end
 
+function generateWordScoresgMatch(inputfile)
+    -- Open input file in read mode
+    local f = assert(io.open(inputfile, "r"))
+
+    -- Read the entire input file as one big string
+    local t = f:read("*all")
+
+    -- Dictionary to store a word and how many times it occurred in the input text file
+    local wordsAndOccurrences = {}
+
+    -- Loop through each word in the input text file finding all "words" with numbers, letters, apostrophes, single quotes (left and right) 
+    for word in string.gmatch(t, "[%d%a'‘’]+") do
+        -- Set word to lowercase because all words in socialsent.csv are in lowercase
+        word = string.lower(word)
+
+        -- If the found word has a score in the social sentiment table then process the wordsAndOccurrences dictionary
+        if gSocialSentimentScores[word] then
+            -- If the word (which has a known corresponding score) has not yet been found, set the number of occurrences to 1
+            if wordsAndOccurrences[word] == nil then
+                -- Set number of occurrences to 1
+                wordsAndOccurrences[word] = 1
+            -- if the word (which has a known corresponding score) has been found fore, increment the number of occurrences
+            else
+                wordsAndOccurrences[word] = wordsAndOccurrences[word] + 1
+            end
+
+            -- Increment number of words found that also have a score for file writing purposes
+            gWordsWithScoresFound = gWordsWithScoresFound + 1
+        end
+
+        -- Increment total word count for file writing purposes
+        gTotalWordCount = gTotalWordCount + 1
+    end
+
+    -- Return words and occurrences dictionary
+    return wordsAndOccurrences
+end
+
 -- Function that calculated the actual sentiment score the input file
-function calculateSocialSentimentScore(wordsAndOccurrences, filename)
+function calculateSocialSentimentScore(wordsAndOccurrences, inputfile)
     -- Create output file with "output_" concatenated to front of filename
-    filename = "output_"..filename
-    local f = assert(io.open(filename, "w"))
+    local outputfile = "output_"..inputfile
+    local f = assert(io.open(outputfile, "w"))
 
     -- Variable to store total sentiment score
     local totalSentimentScore = 0
@@ -153,17 +203,17 @@ function calculateSocialSentimentScore(wordsAndOccurrences, filename)
     print("[word: word_score, accumulated_score]")
     f:write("[word: word_score, accumulated_score]\n")
     
-    for i in pairs(wordsAndOccurrences) do
-        -- If the key "i" exists and the number of occurences for the word "i" is at least one, process value for sentiment score analysis
-        while i and wordsAndOccurrences[i] > 0 do
+    for word, occurences in pairs(wordsAndOccurrences) do
+        -- If the number of occurrences for the word is at least 1, process the score
+        while occurences > 0 do
             -- Add sentiment score based on previous sentiment score and score of the word
-            totalSentimentScore = totalSentimentScore + gSocialSentimentScores[i]
+            totalSentimentScore = totalSentimentScore + gSocialSentimentScores[word]
 
             -- Decrement the number of occurrences for the word
-            wordsAndOccurrences[i] = wordsAndOccurrences[i] - 1
+            occurences = occurences - 1
 
             -- Create formatted string to print and save to output file
-            local formattedString = string.format("%s: %.2f, %.2f", i, gSocialSentimentScores[i], totalSentimentScore)
+            local formattedString = string.format("%s: %.2f, %.2f", word, gSocialSentimentScores[word], totalSentimentScore)
             print(formattedString)
             f:write(formattedString, "\n")
         end
@@ -201,29 +251,7 @@ function getStarRating()
     gStarRating = starRating
 end
 
--- Main function to handle program start
-function main()
-    -- Local variable to store input file name
-    local inputfile = ""
-
-    -- Check if the user provided a file to analyze
-    if arg[1] then
-        -- Set the input file to analyze to the first argument provided by user
-        inputfile = arg[1]
-    -- If the user did not provide a file to analyze, analyze NovelReview.txt
-    else
-        inputfile = "NovelReview.txt"
-    end
-
-    -- Build the social sentiment table baesd on "socialsent.csv"
-    buildSocialSentimentTable("socialsent.csv")
-
-    -- Get the social sentiment score based on user input file
-    getSocialSentimentScore(inputfile)
-
-    -- Get the star rating based on the social sentiment score for the input file
-    getStarRating()
-
+function outputAnalysisDetails(inputfile)
     -- Formatted score variable
     local formattedScore = string.format("%s score: %.2f", inputfile, gAccumulatedSocialSentScore)
     -- Formatter rating variable
@@ -236,8 +264,10 @@ function main()
 
     -- Create output file
     local outputFilename = "output_"..inputfile
+
     -- Open output file
     local f = assert(io.open(outputFilename, "a"))
+
     -- Write summary (score and rating) to output file
     f:write("\n", formattedScore, "\n")
     f:write(formattedRating, "\n")
@@ -252,6 +282,34 @@ function main()
 
     -- Close output file
     f:close()
+end
+
+function checkArgsAndGetInputfile()
+    -- if the user provided an input file to analyze, return it
+    if arg[1] then
+        return arg[1]
+    -- If the user did not provide a file to analyze, return NoveReview.txt
+    else
+        return "NovelReview.txt"
+    end
+end
+
+-- Main function to handle program start
+function main()
+    -- Local variable to store input filename
+    local inputfile = checkArgsAndGetInputfile()
+
+    -- Build the social sentiment table baesd on "socialsent.csv"
+    buildSocialSentimentTable("socialsent.csv")
+
+    -- Get the social sentiment score based on user input file
+    getSocialSentimentScore(inputfile)
+
+    -- Get the star rating based on the social sentiment score for the input file
+    getStarRating()
+
+    -- Print and write the analysis details to the CLI and to a generated output file
+    outputAnalysisDetails(inputfile)
 end
 
 main()
